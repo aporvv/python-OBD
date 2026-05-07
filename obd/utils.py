4fbe77b6e4999ceca711ac93356565f4ef84185a
Szymon Głuch <81253710+aporvv@users.noreply.github.com>
2026-05-07 22:43:52 +0200

    Add support for ttyACM ports on Linux and Cygwin

--- obd/utils.py
+++ obd/utils.py
@@ -172,6 +172,7 @@ def scan_serial():
     if sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
         possible_ports += glob.glob("/dev/rfcomm[0-9]*")
         possible_ports += glob.glob("/dev/ttyUSB[0-9]*")
+        possible_ports += glob.glob("/dev/ttyACM[0-9]*")
 
     elif sys.platform.startswith('win'):
         possible_ports += [r"\\.\COM%d" % i for i in range(256)]
